3c3d001122e407e8c8ab428d3c821645fa475771
Ramon Santos <ramon.santos@viaexpressa.local>
2021-02-24 18:47:01 -0300

    Criado as tabelas de users e users_social

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+						$table->string('cpf', 11)->unique()->nullable();
+						$table->string('name', 50);
+						$table->char('phone', 11);
+						$table->date('birth')->nullable;
+						$table->char('gender', 1)->nullable;
+						$table->text('notes')->nullable;
+
+						$table->string('email', 80)->unique();
+						$table->string('password', 254)->nullable();
+
+						$table->string('status')->default('ativo');
+						$table->string('permission')->default('app.user');
+
+						$table->rememberToken();
+            $table->timestamps();
+						$table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
